781162d381f61bd1917fbd3bff45e0be66755fd0
davidyang2149 <davidyang2149@gmail.com>
2020-11-22 01:12:37 +0900

    Add Recipe viewPage
    Oh My Baking Recipe(레시피 뷰페이지 구현)
    - 레시피 뷰페이지 목업 작성.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,148 @@
 import React from 'react';
 
-export default function App() {
+import categories from '../fixtures/categories';
+
+import recipeMock from '../fixtures/recipe';
+
+export default function App({ recipe = recipeMock }) {
+  const {
+    name,
+    category,
+    product,
+    bakingTemperature,
+    bakingTime,
+    ingredients,
+    process,
+  } = recipe;
   return (
-    <h1>Oh My Baking Recipe</h1>
+    <>
+      <h1>Oh My Baking Recipe</h1>
+      <div>
+        <label htmlFor="recipe-name">
+          레시피명
+        </label>
+        <input
+          id="recipe-name"
+          name="name"
+          value={name}
+          readOnly
+        />
+      </div>
+      <div>
+        <label htmlFor="recipe-category">
+          카테고리
+        </label>
+        <select
+          id="recipe-category"
+          name="category"
+          value={category}
+          readOnly
+        >
+          {
+            categories.map((kind) => (
+              <option
+                key={kind}
+                value={kind}
+              >
+                {kind}
+              </option>
+            ))
+          }
+        </select>
+      </div>
+      <div>
+        <label htmlFor="recipe-product">
+          생산량
+        </label>
+        <input
+          id="recipe-product"
+          name="product"
+          value={product}
+          readOnly
+        />
+        &nbsp;
+        개
+      </div>
+      <div>
+        <label htmlFor="recipe-bakingTemperature">
+          오븐 온도
+        </label>
+        <input
+          id="recipe-bakingTemperature"
+          name="bakingTemperature"
+          value={bakingTemperature}
+          readOnly
+        />
+        &nbsp;
+        ℃
+        &nbsp;
+        <label htmlFor="recipe-bakingTime">
+          굽는 시간
+        </label>
+        <input
+          id="recipe-bakingTime"
+          name="bakingTime"
+          value={bakingTime}
+          readOnly
+        />
+        &nbsp;
+        분
+      </div>
+      <ul>
+        {
+          ingredients.length !== 0 && (
+            ingredients.map(({ ingredientName, weight }) => (
+              <li key={ingredientName}>
+                <label htmlFor="recipe-ingredientName">
+                  재료
+                </label>
+                <input
+                  id="recipe-ingredientName"
+                  name="ingredientName"
+                  value={ingredientName}
+                  readOnly
+                />
+                &nbsp;
+                <input
+                  id="recipe-weight"
+                  name="weight"
+                  value={weight}
+                  readOnly
+                />
+                g
+              </li>
+            ))
+          )
+        }
+        <li>
+          <label htmlFor="recipe-ingredient-new">
+            재료
+          </label>
+          <input
+            id="recipe-ingredientName-new"
+            name="ingredientName"
+            readOnly
+          />
+          &nbsp;
+          <input
+            id="recipe-weight-new"
+            name="weight"
+            readOnly
+          />
+          g
+        </li>
+      </ul>
+      <div>
+        <label htmlFor="recipe-process">
+          만드는 방법
+        </label>
+        <textarea
+          id="recipe-process"
+          name="process"
+          value={process}
+          readOnly
+        />
+      </div>
+    </>
   );
 }
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { render } from '@testing-library/react';
+
 import App from './App';
 
 describe('App', () => {
@@ -10,5 +11,23 @@ describe('App', () => {
     ));
 
     expect(container).toHaveTextContent('Oh My Baking Recipe');
+  });
+
+  context('when recipe view page', () => {
+    it('renders with recipe information', () => {
+      const { container } = render((
+        <App />
+      ));
+
+      expect(container).toHaveTextContent('레시피명');
+      expect(container).toHaveTextContent('카테고리');
+      expect(container).toHaveTextContent('생산량');
+
+      expect(container).toHaveTextContent('오븐 온도');
+      expect(container).toHaveTextContent('굽는 시간');
+
+      expect(container).toHaveTextContent('재료');
+      expect(container).toHaveTextContent('만드는 방법');
+    });
   });
 });
